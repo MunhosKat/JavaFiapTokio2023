bbe88fa80833b9511ae8e0bc46109d067a68933b
MunhosKat <matheus.munhos2005@gmail.com>
2023-04-13 18:25:08 -0300

    primeiro commit java logica

---
+++ munhos/src/logica/aulas/PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+
+package logica.aulas;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+	 System.out.println("Olá Mundo");
+
+	 int idade;
+	 idade = 23;
+	 
+	 System.out.println(idade);
+	 
+	 idade =42;
+	 System.out.println(idade);
+	 
+	 double salario =1318.20;
+	 System.out.println(salario);
+	 
+	 String nome ="Matheus";
+	 System.out.println(nome);
+	 
+	 System.out.println("Bem-Vindo, " + nome + "!");
+	 System.out.println(nome + ", você tem " + (idade+idade));
+	 
+	 
+	}
+
+}
---
+++ munhos/src/logica/aulas/SegundoPrograma.java
@@ -0,0 +1,24 @@
+package logica.aulas;
+
+import java.util.Scanner;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome:");
+		String nome = entrada.nextLine();
+		
+		
+		
+		System.out.println("Digite sua Idade");
+		System.out.println("Sua Idade e:" + entrada.nextInt());
+		
+		
+		System.out.println("Bem-vindo, " + nome);
+
+	}
+
+}
